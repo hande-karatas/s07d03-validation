194f81f31673ab326372ebd2c8207cd3b0f456bf
hande-arch <156059093+hande-karatas@users.noreply.github.com>
2025-03-25 23:30:50 +0300

    login sayfası düzenlendi
    3. gün ödevinden alındı

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  FormFeedback,
+} from 'reactstrap';
 import { useHistory } from 'react-router-dom';
-
 import axios from 'axios';
 
 const initialForm = {
@@ -10,33 +16,71 @@ const initialForm = {
   terms: false,
 };
 
+const errorMessages = {
+  email: 'Please enter a valid email address',
+  password: 'Password must be at least 4 characters long',
+};
+
 export default function Login() {
   const [form, setForm] = useState(initialForm);
+  const [isValid, setIsValid] = useState(false);
+  const [errors, setErrors] = useState({});
 
   const history = useHistory();
 
+  useEffect(() => {
+    validateForm();
+  }, [form]);
+
+  const validateEmail = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
+  const validateForm = () => {
+    let newErrors = {};
+
+    if (!validateEmail(form.email)) {
+      newErrors.email = errorMessages.email;
+    }
+
+    if (form.password.length < 4) {
+      newErrors.password = errorMessages.password;
+    }
+
+    if (!form.terms) {
+      newErrors.terms = 'You must accept the terms.';
+    }
+
+    setErrors(newErrors);
+    setIsValid(Object.keys(newErrors).length === 0);
+  };
+
   const handleChange = (event) => {
     let { name, value, type, checked } = event.target;
-    value = type == 'checkbox' ? checked : value;
-    setForm({ ...form, [name]: value });
+    value = type === 'checkbox' ? checked : value;
+
+    setForm((prevForm) => ({ ...prevForm, [name]: value }));
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    axios
-      .get('https://6540a96145bedb25bfc247b4.mockapi.io/api/login')
-      .then((res) => {
-        const user = res.data.find(
-          (item) => item.password == form.password && item.email == form.email
-        );
-        if (user) {
-          setForm(initialForm);
-          history.push('/main');
-        } else {
-          history.push('/error');
-        }
-      });
+    if (isValid) {
+      axios
+        .get('https://6540a96145bedb25bfc247b4.mockapi.io/api/login')
+        .then((res) => {
+          const user = res.data.find(
+            (item) =>
+              item.password === form.password && item.email === form.email
+          );
+          if (user) {
+            setForm(initialForm);
+            history.push('/main');
+          } else {
+            history.push('/error');
+          }
+        });
+    }
   };
 
   return (
@@ -50,34 +94,41 @@ export default function Login() {
           type="email"
           onChange={handleChange}
           value={form.email}
+          invalid={!!errors.email}
         />
+        {errors.email && <FormFeedback>{errors.email}</FormFeedback>}
       </FormGroup>
+
       <FormGroup>
         <Label for="examplePassword">Password</Label>
         <Input
           id="examplePassword"
           name="password"
-          placeholder="Enter your password "
+          placeholder="Enter your password"
           type="password"
           onChange={handleChange}
           value={form.password}
+          invalid={!!errors.password}
         />
+        {errors.password && <FormFeedback>{errors.password}</FormFeedback>}
       </FormGroup>
-      {/* reactstrap checkbox ekleyelim*/}
+
       <FormGroup check>
         <Input
-          type="checkbox"
-          name="terms"
           id="terms"
+          name="terms"
           checked={form.terms}
+          type="checkbox"
           onChange={handleChange}
-        />{' '}
+        />
         <Label htmlFor="terms" check>
-          I agree to terms of service and privacy policy{' '}
+          I agree to terms of service and privacy policy
         </Label>
+        {errors.terms && <FormFeedback>{errors.terms}</FormFeedback>}
       </FormGroup>
+
       <FormGroup className="text-center p-4">
-        <Button color="primary" disabled={!form.terms}>
+        <Button color="primary" disabled={!isValid}>
           Sign In
         </Button>
       </FormGroup>
